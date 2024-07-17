1c5a6179fbb41a24e8ea1b7ae89068c2b212cedb
acepriority <sudaicejeymack@gmail.com>
2024-07-17 18:01:29 +0300

    House and Property Models

--- Management/Property/models.py
+++ Management/Property/models.py
@@ -27,21 +27,48 @@ class LandLord(models.Model):
         return f"LandLord: {self.person}"
 
 
+class Property(models.Model):
+    location = models.CharField(
+        max_length=255
+        )
+    landlord = models.ForeignKey(
+        LandLord,
+        on_delete=models.CASCADE,
+        related_name='properties'
+        )
+
+    def __str__(self):
+        return f"Property: {self.location}, Landlord: {self.landlord}"
+
+
+class House(models.Model):
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name='houses')
+    name = models.CharField(
+        max_length=100
+        )
+    rent = models.IntegerField()
+    tenant = models.ForeignKey(
+        'Tenant',
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='houses')
+
+    def __str__(self):
+        return f"House: {self.name}, Property: {self.property}"
+
+
 class Tenant(models.Model):
     person = models.OneToOneField(
         Person,
         on_delete=models.CASCADE,
-        related_name='tenant'
-        )
+        related_name='tenant')
     landlord = models.ForeignKey(
         LandLord,
-        on_delete=models.CASCADE
-        )
-    house_id = models.CharField(
-        max_length=100,
-        unique=True
-        )
-    rent = models.IntegerField()
+        on_delete=models.CASCADE,
+        related_name='tenants')
 
     def __str__(self):
-        return f"Tenant: {self.person}, House ID: {self.house_id}"
+        return f"Tenant: {self.person}"
